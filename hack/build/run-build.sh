540e84ed7fae52ab78913c0815ed555daa1f08c2
Priti Kumari <pkumari@redhat.com>
2024-02-19 10:50:01 +0530

    Fix shellcheck error

--- hack/build/run-build.sh
+++ hack/build/run-build.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Default values
 default_git_repo_url=https://github.com/devfile-samples/devfile-sample-go-basic
 default_git_revision=main
 default_docker_filepath=docker/Dockerfile
-
+default_config_file="${SCRIPT_DIR}/build-config.env"
 default_pipeline=docker-build-rhtap
 
 
 # Load build credentials and configuration
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-config_file="${SCRIPT_DIR}/build-config.env"
-if [ ! -f $config_file ]; then
+CONFIG_FILE="${CONFIG_FILE:-$default_config_file}"
+if [ ! -f "${CONFIG_FILE}" ]; then
   echo "$SCRIPT_DIR/build-config.env does not exists. Look at instructions in $SCRIPT_DIR/build-config-template.env"
   exit 1
 fi
 # Get IMAGE_REPOSITORY and ROX_ENDPOINT
-source "$config_file"
-
+# shellcheck source=/dev/null
+source "${CONFIG_FILE}"
 
 GIT_REPO_URL="${GIT_REPO_URL:-$default_git_repo_url}"
 GIT_REVISION="${GIT_REVISION:-$default_git_revision}"
@@ -26,11 +27,11 @@ DOCKER_FILEPATH="${DOCKER_FILEPATH:-$default_docker_filepath}"
 
 PIPELINE="${PIPELINE:-$default_pipeline}"
 
-if [ $ROX_ENDPOINT == 'in-cluster' ] ; then
+if [ "$ROX_ENDPOINT" == 'in-cluster' ] ; then
   ROX_ENDPOINT=$(oc get route central -o jsonpath='{.spec.host}' -n rhacs-operator):443
 fi
 
-oc delete pipelinerun $PIPELINE &> /dev/null
+oc delete pipelinerun "$PIPELINE" &> /dev/null
 echo "Running pipeline $PIPELINE to build $GIT_REPO_URL @ $GIT_REVISION into $OUTPUT_IMAGE"
 
 cat <<EOF | oc apply -f -
